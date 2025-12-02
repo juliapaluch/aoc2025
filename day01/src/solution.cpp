900723bb22407ad585a590399d17dec19a843327
Julia Paluch <paluchj4@gmail.com>
2025-12-01 22:57:22 -0500

    too low for part2

--- day01/src/solution.cpp
+++ day01/src/solution.cpp
@@ -19,21 +19,34 @@ class Lock {
 public:
 	int64_t dial;
 	int64_t password;
+	int64_t actualPassword;
 
-	Lock() { dial = 50; password = 0; }
+	Lock() { dial = 50; password = 0; actualPassword = 0; }
 
 	void turn(std::string rotation) {
 		char direction = rotation.at(0);
 		int amount = std::stoi(rotation.substr(1));
+		int64_t dialBefore = dial;
+		std::cout << "The rotation requested is " << rotation << std::endl;
 		switch (direction) {
 			case 'L': dial -= amount; break;
 			case 'R': dial += amount; break;
 			default: std::cout << "wtf" << std::endl;
 		}
+
 		normalize();
+
+		if (amount > 100) { actualPassword += amount / 100; }
+		else if (amount < 100) {
+			if ((direction == 'L' && dial > dialBefore) || (direction == 'R' && dial < dialBefore)) {
+				actualPassword++;
+			}
+		}
+		
+		if (dial == 0) password++;
 		std::cout << "The dial is now at " << std::to_string(dial) << std::endl;
 		std::cout << "The password is now at " << std::to_string(password) << std::endl;
-		if (dial == 0) password++;
+		std::cout << "The actual password is now at " << std::to_string(actualPassword) << std::endl;
 	}
 
 private:	
@@ -48,11 +61,11 @@ Solution solve(std::vector<std::string> input) {
 	for (std::string rotation : input) {
 		lock.turn(rotation);
 	}
-	return Solution(lock.password, 0);
+	return Solution(lock.password, lock.actualPassword);
 }
 
 int main(int argc, char* argv[]) {
-	std::ifstream infile("input/input.txt");
+	std::ifstream infile(argv[1]);
 	std::string line;
 	std::vector<std::string> input;
 	while (std::getline(infile, line)) {
