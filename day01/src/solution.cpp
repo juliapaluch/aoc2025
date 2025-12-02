0e555fd588e8b9c53cf3d8fc1b790f15668a0aa0
Julia Paluch <paluchj4@gmail.com>
2025-12-01 20:03:49 -0400

    add day 1 part 1 solution

--- day01/src/solution.cpp
+++ day01/src/solution.cpp
@@ -2,10 +2,39 @@
 #include <fstream>
 #include <iostream>
 
+class Lock {
+public:
+	int dial;
+	int password;
+
+	Lock() { dial = 50; password = 0; }
+
+	void turn(std::string rotation) {
+		char direction = rotation.at(0);
+		int amount = std::stoi(rotation.substr(1));
+		switch (direction) {
+			case 'L': dial -= amount; break;
+			case 'R': dial += amount; break;
+			default: std::cout << "wtf" << std::endl;
+		}
+		normalize();
+		std::cout << "The dial is now at " << std::to_string(dial) << std::endl;
+		if (dial == 0) password++;
+	}
+
+private:	
+	void normalize() { 
+		dial %= 100;
+		if (dial < 0) dial += 100;
+	}
+};
 
 uint64_t part1(std::vector<std::string> input) {
-	uint64_t solution = 0;
-	return solution;
+	Lock lock;
+	for (std::string rotation : input) {
+		lock.turn(rotation);
+	}
+	return lock.password;
 }
 
 uint64_t part2(std::vector<std::string> input) {
